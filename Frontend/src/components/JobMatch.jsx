b710a4e66ab67a484cd5685fb6ebe43b035d7fae
deepanshu yadav <DeepanshuYadav7111@gmail.com>
2025-08-23 17:22:54 +0530

    upload

--- Frontend/src/components/JobMatch.jsx
+++ Frontend/src/components/JobMatch.jsx
@@ -9,7 +9,7 @@ const JobMatch = ({ resumeText }) => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await axios.post("https://resume-analyzer-backend-7v08.onrender.com/api/match-jobs", {
+        const res = await axios.post("https://resume-analyzer-backend-navy.vercel.app/api/match-jobs", {
           resumeText,
         });
         setJobs(res.data);
