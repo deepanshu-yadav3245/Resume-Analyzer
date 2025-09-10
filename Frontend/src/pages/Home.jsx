59caecd4e3fbdaa2bcb4a7859228e76f03a55afd
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-10 11:01:22 +0530

    update

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
       {/*  Background blobs for dark mode */}
       {darkMode && (
         <>
-          <div className="absolute top-0 left-0 w-72 h-72 bg-blue-500/20 rounded-full blur-3xl animate-pulse z-0"></div>
+          <div className="absolute top-0 left-0 w-73 h-72 bg-blue-500/20 rounded-full blur-3xl animate-pulse z-0"></div>
           <div className="absolute bottom-0 right-0 w-96 h-96 bg-purple-600/20 rounded-full blur-3xl animate-pulse z-0"></div>
         </>
       )}
