201f2172c1274b3ad27d603286c00db5d4284fa1
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-18 21:16:35 +0530

    update

--- Backend/services/upworkService.js
+++ Backend/services/upworkService.js
@@ -19,7 +19,7 @@ async function getJobs(resumeSkills = []) {
     },
   ];
 
-  const formatted = jobs.map((job) => {
+  const formatted = .map((job) => {
     const matchScore = calculateMatchScore(
       resumeSkills.map((s) => s.toLowerCase()),
       job.requiredSkills.map((s) => s.toLowerCase())
